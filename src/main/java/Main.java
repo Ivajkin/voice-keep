73647d959f3066e5610ceb262aa576711e29a571
Timothy S. Ivaykin M.E <t.s.ivaykin@gmail.com>
2017-03-16 00:32:14 +1000

    Testing enabled CORS requests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -137,7 +137,7 @@ public class Main {
       return output;
     });
 
-    enableCORS("jsbin.com", "GET", "");
+    enableCORS("http://null.jsbin.com", "GET", "");
   }
   // Enables CORS on requests. This method is an initialization method and should be called once.
   private static void enableCORS(final String origin, final String methods, final String headers) {
